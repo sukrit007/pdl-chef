dde9ea2d3322b6b428f459e92ffe9ac363560151
sukrit007 <sukrit007@gmail.com>
2012-06-18 02:29:27 -0700

    metadata modification

--- cookbooks/test/metadata.rb
+++ cookbooks/test/metadata.rb
@@ -4,3 +4,4 @@ license          "All rights reserved"
 description      "Installs/Configures test"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+recipe "test::default","Test Recipe"  
