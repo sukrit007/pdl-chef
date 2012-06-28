10448c6d959f1dbf077d26c8fefe2e2f3e4c56a0
sukrit007 <sukrit007@gmail.com>
2012-06-28 13:25:58 -0700

    cache_version : remove (0.10 issue)

--- cookbooks/postgres/recipes/install.rb
+++ cookbooks/postgres/recipes/install.rb
@@ -24,7 +24,7 @@ when "centos"
     action :install
     source "/tmp/pgdg.rpm"
     provider Chef::Provider::Package::Rpm
-    flush_cache :after => true
+    #flush_cache :after => true
   end   
    
 when "ubuntu"
