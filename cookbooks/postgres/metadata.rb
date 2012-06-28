8be56426395bc2afdad42bfa89bca9a3cbfa0aeb
sukrit007 <sukrit007@gmail.com>
2012-06-28 13:15:29 -0700

    install instead of upgrade

--- cookbooks/postgres/metadata.rb
+++ cookbooks/postgres/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "sukrit.khera@pearson.com"
 license          "All rights reserved"
 description      "Installs/Configures postgres"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 supports "centos" 
 recipe "postgres::install","Install PostgreSQL Server"  
