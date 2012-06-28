c2c3c088448f8595a9809a03f8afda13bfc3da4c
sukrit007 <sukrit007@gmail.com>
2012-06-28 14:33:09 -0700

    CentOS postgresql package name update

--- cookbooks/postgres/attributes/install.rb
+++ cookbooks/postgres/attributes/install.rb
@@ -1,7 +1,9 @@
-default["postgresql"]["package"]="postgresql-9.1"
+
 
 case node["platform"] 
   when "centos"
-    default["pgdg"]="pgdg-centos91-9.1-4.noarch.rpm"  
+    default["pgdg"]="pgdg-centos91-9.1-4.noarch.rpm"
+    default["postgresql"]["package"]="postgresql91*"  
   when "ubuntu"
+    default["postgresql"]["package"]="postgresql-9.1"
 end
